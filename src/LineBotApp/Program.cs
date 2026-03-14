4d1ef4d759ef4103bfccb60ec4aab71ddb7005f2
Yuta Matsumura <tsubakimoto@users.noreply.github.com>
2026-03-15 00:27:58 +0900

    Initial commit

---
+++ src/LineBotApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
+//builder.Services
+//    .AddApplicationInsightsTelemetryWorkerService()
+//    .ConfigureFunctionsApplicationInsights();
+
+// LINE Messaging API
+builder.Services.AddHttpClient("LineMessagingApi", client =>
+{
+    client.BaseAddress = new Uri("https://api.line.me/");
+    client.DefaultRequestHeaders.Accept.Add(new("application/json"));
+});
+builder.Services.AddHttpClient("LineContentApi", client =>
+{
+    client.BaseAddress = new Uri("https://api-data.line.me/");
+    client.DefaultRequestHeaders.Accept.Add(new("application/json"));
+});
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Build().Run();
